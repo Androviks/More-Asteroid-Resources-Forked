eaf3babf38c28d1844803e50980c45b0c53de626
Androviks <piejohnson69@gmail.com>
2024-11-15 05:39:58 -0800

    Update tables.lua
    Fix missing item, syntax errors

--- tables.lua
+++ tables.lua
@@ -5,14 +5,15 @@ local tables = {}
 tables.asteroids = {"metallic", "carbonic", "oxide"}
 tables.target_items = {
     metallic = {
-        {item = "holmium-ore", default_probability = {[""] = 0.001, ["advanced-"] = 0.01}},
+        {item = "holmium-ore", default_probability = {[""] = 0.03, ["advanced-"] = 0.1}}.
+        {item = "tungsten-ore", default_probability = {[""] = 0.001, ["advanced-"] = 0.01}}.
     },
     carbonic = {
         {item = "stone", default_probability = {[""] = 0.03, ["advanced-"] = 0.1}},
-        {item = "uranium-ore", default_probability = {[""] = 0.001, ["advanced-"] = 0.01}},
+        {item = "uranium-ore", default_probability = {[""] = 0.001, ["advanced-"] = 0.01}}
     },
     oxide = {
         {item = "stone", default_probability = {[""] = 0.03, ["advanced-"] = 0.1}},
-        {item = "iron-ore", default_probability = {[""] = 0.001, ["advanced-"] = 0.01}},
+        {item = "iron-ore", default_probability = {[""] = 0.001, ["advanced-"] = 0.01}}
     },
 }
