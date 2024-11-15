c1c4e7581eddc391b6cf4d965c72a1a876e2ace4
Androviks <piejohnson69@gmail.com>
2024-11-15 06:33:00 -0800

    Update data.lua

--- data.lua
+++ data.lua
@@ -13,7 +13,7 @@ if settings.startup["processing-mode"].value ~= "standalone-only" then
                             type = "item",
                             name = target_item.item,
                             amount = settings.startup[target_item.item .. "-amount-" .. asteroid].value,
-                            probability = settings.startup["advanced-" .. target_item.item .. "-probability"].value
+                            probability = settings.startup["advanced-" .. target_item.item .. asteroid .. "-probability"].value
                         })
                     end
                 end
