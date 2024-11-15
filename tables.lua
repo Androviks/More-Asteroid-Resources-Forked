17d5074f18af08b4e91024367ed1e4fdd45b2122
Androviks <piejohnson69@gmail.com>
2024-11-15 04:01:17 -0800

    Update tables.lua
    Diversification of resources on chunk type.

--- tables.lua
+++ tables.lua
@@ -4,9 +4,15 @@ local tables = {}
 
 tables.asteroids = {"metallic", "carbonic", "oxide"}
 tables.target_items = {
-    {item = "stone", default_probability = {[""] = 0.05, ["advanced-"] = 0.3},
-    {item = "uranium-ore", default_probability = {[""] = 0.001, ["advanced-"] = 0.01}
+    metallic = {
+        {item = "holmium-ore", default_probability = {[""] = 0.001, ["advanced-"] = 0.01}},
+    },
+    carbonic = {
+        {item = "stone", default_probability = {[""] = 0.03, ["advanced-"] = 0.1}},
+        {item = "uranium-ore", default_probability = {[""] = 0.001, ["advanced-"] = 0.01}},
+    },
+    oxide = {
+        {item = "stone", default_probability = {[""] = 0.03, ["advanced-"] = 0.1}},
+        {item = "iron-ore", default_probability = {[""] = 0.001, ["advanced-"] = 0.01}},
+    },
 }
-tables.prefixes = {"", "advanced-"}
-
-return tables
