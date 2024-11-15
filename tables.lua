a557200df3ee2634d0b836e2afdd214bd7f4eda1
Androviks <piejohnson69@gmail.com>
2024-11-15 01:46:15 -0800

    Syntax change for standalone recipes

--- tables.lua
+++ tables.lua
@@ -7,6 +7,6 @@ tables.target_items = {
     {item = "stone", default_probability = {[""] = 0.05, ["advanced-"] = 0.3, ["standalone-"] = 0.5}},
     {item = "uranium-ore", default_probability = {[""] = 0.001, ["advanced-"] = 0.01, ["standalone-"] = 0.1}}
 }
-tables.prefixes = {"", "advanced-", "standalone-"}
+tables.prefixes = {"", "advanced-", "trace-"}
 
 return tables
